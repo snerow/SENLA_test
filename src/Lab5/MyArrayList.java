5b3c7d9bfacc9ea6eb4308419e93802262fb8ae3
snerow <148005736+snerow@users.noreply.github.com>
2024-06-05 21:30:45 +0300

    Update MyArrayList.java

--- src/Lab5/MyArrayList.java
+++ src/Lab5/MyArrayList.java
@@ -1,2 +1,24 @@
-package Lab5;public class MyArrayList {
+package Lab5;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MyArrayList extends ArrayList {
+    private ArrayList<Object> objects = new ArrayList<>();
+
+    public void inputObjects()
+    {
+        Scanner in = new Scanner(System.in);
+
+        long number_of_items = in.nextLong();
+        for (long i = 0; i < number_of_items; i++)
+        {
+            Object temporary_obj = new Object(in.nextLong(), in.nextLong());
+            this.objects.add(temporary_obj);
+        }
+        for (Object obj : this.objects)
+        {
+            obj.printObject();
+        }
+    }
 }
